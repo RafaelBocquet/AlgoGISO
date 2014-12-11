9ab2149f019166fba5984d0cb645f0272f75fa05
Rafaël Bocquet <rafaelbocquet+github@gmail.com>
2014-12-11 13:33:56 +0100

    .

--- set.h
+++ set.h
@@ -14,7 +14,7 @@ typedef set_node* int_set;
 int_set int_set_empty();
 void int_set_free(int_set* s);
 void int_set_print(int_set* s);
-void int_set_insert(int_set* s, int v);
+bool int_set_insert(int_set* s, int v);
 int int_set_delete(int_set* s);
 
 #endif
